bbf20aae83bfedb16c2e2acab148d1dce222184c
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2017-11-12 23:14:41 -0500

    Imports the main components for the landing page

--- yojo_site/src/App.js
+++ yojo_site/src/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Meditation from './Meditation';
+import Move from  './Move';
+import Breath from  './Breath';
+import VideoCarousel from './VideoCarousel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">
             <img className="landing-logo" src="https://cdn.sproutvideo.com/7eab0df30ffe4012795c6db7e71fcddc/logos/site_logo_1509484297.png" alt="logo" />
           </h1>
         </header>
-        <div className="video-outer-container">
-          <div className="video-frame">
-            <div className="video-container">
-              <iframe
-                src="https://videos.sproutvideo.com/embed/a09addb8161defcf28/5fbef75aaacf52b8?signature=PhxIM3rtvA7WWhBgB4R6ns%2F66gY%3D&amp;access_grant_id=489ddcbf101ae1c3c0&amp;downloads=&amp;expires=1510521151&amp;has_access=true&amp;login_id=789dd1b5131ff6&amp;type=hd&amp;vemail=melissa.f.wimberly@gmail.com"
-                width="100%"
-                height="100%"
-                // frameborder="0"
-                className="sproutvideo-player"
-                // allowfullscreen=""
-                > </iframe>
-            </div>
-          </div>
+        <VideoCarousel />
+        <div className="category-wrapper">
+          <Meditation />
+          <Move />
+          <Breath />
         </div>
       </div>
     );
